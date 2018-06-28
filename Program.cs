7bb038a97920723a9b41a00209fdd3662c0d42f9
Rolf Shroff <rolf.shroff@outlook.com>
2018-06-28 11:01:00 +1000

    Added comments to generate XML documentation

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    /// <summary>
+    /// Program summary
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main method summary
+        /// </summary>
+        /// <param name="args">Pass an argument array to the Main method</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
